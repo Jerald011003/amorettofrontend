a0fb7e528ad19fb5025f99ea747b007528b3c104
alaic19 <alaicapineda2@gmail.com>
2023-04-26 23:27:52 +0800

    asdsaasds

--- src/screens/OrderListScreen.js
+++ src/screens/OrderListScreen.js
@@ -54,12 +54,12 @@ function OrderListScreen({ history }) {
                                     <tr key={order._id}>
                                         {/* <td>{order._id}</td> */}
                                         {/* <td>{order.user && order.user.name}</td> */}
-                                        <td></td>
+                                        <td>{order.createdAt?.substring(0, 10)}</td>
                                         <td>${order.totalPrice}</td>
 
                                         <td>{order.isPaid ? (
-                                            order.paidAt.substring(0, 10)
-                                        ) : (
+    order.paidAt?.substring(0, 10)
+    ) : (
                                                 <i className='fas fa-times' style={{ color: 'red' }}></i>
                                             )}
                                         </td>
