4d7f44346e1c93307803038696319347193fc29d
alaic19 <alaicapineda2@gmail.com>
2023-04-26 23:26:22 +0800

    asdsaasds

--- src/screens/OrderListScreen.js
+++ src/screens/OrderListScreen.js
@@ -54,7 +54,7 @@ function OrderListScreen({ history }) {
                                     <tr key={order._id}>
                                         {/* <td>{order._id}</td> */}
                                         {/* <td>{order.user && order.user.name}</td> */}
-                                        <td>{order.createdAt.substring(0, 10)}</td>
+                                        <td></td>
                                         <td>${order.totalPrice}</td>
 
                                         <td>{order.isPaid ? (
